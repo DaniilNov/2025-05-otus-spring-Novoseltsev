1cde811872290cd70af649752be52e790b295b01
xa86 <xa86@mail.ru>
2025-07-24 19:26:37 +0300

    * homework four v1

---
+++ hw04-spring-shell/src/main/java/ru/otus/hw/service/TestRunnerServiceImpl.java
@@ -0,0 +1,31 @@
+package ru.otus.hw.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class TestRunnerServiceImpl implements TestRunnerService {
+
+    private final TestService testService;
+
+    private final StudentService studentService;
+
+    private final ResultService resultService;
+
+    private final LocalizedIOService ioService;
+
+    @Override
+    public void run() {
+        try {
+            var student = studentService.determineCurrentStudent();
+            var testResult = testService.executeTestFor(student);
+            resultService.showResult(testResult);
+        } catch (Exception e) {
+            log.error("Error: {}", e.getMessage(), e);
+            ioService.printLineLocalized("error.message");
+        }
+    }
+}
