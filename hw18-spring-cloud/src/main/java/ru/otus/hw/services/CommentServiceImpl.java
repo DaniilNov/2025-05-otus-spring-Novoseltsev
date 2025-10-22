892e31444b80bc604eae92093552da134591081c
xa86 <xa86@mail.ru>
2025-10-22 09:13:32 +0300

    homework eighteen init

---
+++ hw18-spring-cloud/src/main/java/ru/otus/hw/services/CommentServiceImpl.java
@@ -0,0 +1,60 @@
+package ru.otus.hw.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.otus.hw.exceptions.EntityNotFoundException;
+import ru.otus.hw.models.Book;
+import ru.otus.hw.models.Comment;
+import ru.otus.hw.repositories.BookRepository;
+import ru.otus.hw.repositories.CommentRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class CommentServiceImpl implements CommentService {
+
+    private final CommentRepository commentRepository;
+
+    private final BookRepository bookRepository;
+
+    @Transactional(readOnly = true)
+    @Override
+    public Optional<Comment> findById(String id) {
+        return commentRepository.findById(id);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<Comment> findByBookId(String bookId) {
+        return commentRepository.findByBookId(bookId);
+    }
+
+    @Transactional
+    @Override
+    public Comment create(String text, String bookId) {
+        Book book = bookRepository.findById(bookId)
+                .orElseThrow(() -> new EntityNotFoundException("Book with id %s not found".formatted(bookId)));
+        Comment comment = new Comment();
+        comment.setText(text);
+        comment.setBook(book);
+        return commentRepository.save(comment);
+    }
+
+    @Transactional
+    @Override
+    public Comment update(String id, String text) {
+        Comment comment = commentRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Comment with id %s not found".formatted(id)));
+        comment.setText(text);
+        return commentRepository.save(comment);
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(String id) {
+        commentRepository.deleteById(id);
+    }
+}
